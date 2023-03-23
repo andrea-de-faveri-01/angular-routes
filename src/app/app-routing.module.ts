ae732a8a72ff769de28b6cce36e6aff000866d06
andrea-de-faveri-01 <andreadefaveri01@gmail.com>
2023-03-23 00:11:46 +0000

    frist commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    
+    component: HomeComponent,
+    
+  },
+  {
+    path: 'gallery',
+    
+    component: GalleryComponent,
+    
+  },
+  {
+    path: 'contact',
+    
+    component: ContactComponent,
+    
+  },
+  
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryModule } from './pages/gallery/gallery.module';
+import { HomeModule } from './pages/home/home.module';
+import { ContactModule } from './pages/contact/contact.module';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    AppRoutingModule,
+    HomeModule,
+    GalleryModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
